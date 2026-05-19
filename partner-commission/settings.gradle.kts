7fdbfe435021758632df5fac12fad7eabf4264cb
zionlee <zionlee0927@gmail.com>
2026-05-19 17:57:15 +0900

    feat: partner-commission 프로젝트 초기 설정
    - Kotlin + Spring Boot 3.3 + JPA + PostgreSQL
    - Docker Compose (포트 5433)
    - 메인 애플리케이션 클래스

---
+++ partner-commission/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "partner-commission"
---
+++ partner-commission/src/main/kotlin/com/partnercommission/PartnerCommissionApplication.kt
@@ -0,0 +1,11 @@
+package com.partnercommission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PartnerCommissionApplication
+
+fun main(args: Array<String>) {
+    runApplication<PartnerCommissionApplication>(*args)
+}
